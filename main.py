38932acbce9080573c37ca37dbe8f15b26593cde
kinsmo <yxzhang92@163.com>
2023-04-03 23:51:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -67,10 +67,10 @@ st.dataframe(df)
 def submit():
     if password == '7158':
         # df.to_csv(csv_url, index=False)
-        c = pygsheets.authorize(service_file='snappy-airway-286708-67e493d203e2.json')
-        sh = c.open_by_key('1g6Il9tXoFnqRGS4QnbbeYiyTT_3MjSsOqydZVGuGWlA')
-        wks = sh.worksheet('index', 0)
-        wks.set_dataframe(df,(1,1))
+        gc = pygsheets.authorize(service_file='snappy-airway-286708-67e493d203e2.json')
+        sheet = gc.open('kwork_phd_thesis')
+        worksheet = sheet[0]
+        worksheet.set_dataframe(df,(1,1))
         st.balloons()
         
 with col2:
